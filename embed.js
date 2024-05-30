b47e3d21ba49bc901f26b08d225f5fa83216c0f9
Go24pp <fontanilladioey1@gmail.com>
2024-05-30 18:47:42 +0300

    Add files via upload

---
+++ embed.js
@@ -0,0 +1,17 @@
+(function() {
+  var container = document.getElementById('embedded-content');
+  if (!container) {
+    container = document.createElement('div');
+    container.id = 'embedded-content';
+    document.body.appendChild(container);
+  }
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://black-xi.vercel.app', true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      container.innerHTML = xhr.responseText;
+    }
+  };
+  xhr.send();
+})();
